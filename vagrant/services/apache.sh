e43b611968264717924f95cd11e45e8399578613
Rainzart <hadder@gmail.com>
2017-11-24 14:31:18 -0200

    Adicionado comando sed pra trocar o document root do default
    Trocado quebra de linha do addvhostlarave.sh

--- vagrant/services/apache.sh
+++ vagrant/services/apache.sh
@@ -2,3 +2,6 @@
 
 aptitude install -y apache2
 a2enmod rewrite
+
+echo '[Trocando DocumentRoot do localhost]'
+sed -i -e 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www/g' /etc/apache2/sites-available/000-default.conf
